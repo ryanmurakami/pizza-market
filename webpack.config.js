0125d229fd1e4facf0c330e41c04bbb523a27c62
Ryan Lewis <ryanharrisonlewis@gmail.com>
2017-01-16 08:42:40 -0800

    moving source

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  context: __dirname + "/",
-  entry: "./ui/js/index.js",
+  context: __dirname + '/',
+  entry: './ui/js/index.js',
   output: {
-    path: __dirname + "/ui/public/js",
-    filename: "application.js"
+    path: __dirname + '/ui/public/js',
+    filename: 'application.js'
   }
 };
