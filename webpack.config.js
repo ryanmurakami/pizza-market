962e3bc52a83ee4ee65cdbe11fb9b5e66ef294e9
Ryan Lewis <ryanharrisonlewis@gmail.com>
2017-01-16 08:43:30 -0800

    right folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   context: __dirname + '/',
-  entry: './ui/js/index.js',
+  entry: './src/ui/js/index.js',
   output: {
-    path: __dirname + '/ui/public/js',
+    path: __dirname + '/src/ui/public/js',
     filename: 'application.js'
   }
 };
